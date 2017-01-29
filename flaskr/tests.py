4c508aa8cf2378407e2f9d911f1eb783f3c972f4
Toby Wilkins <tobwilk@gmail.com>
2017-01-29 14:27:23 +0000

    Add skeleton unittest

---
+++ flaskr/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+import flaskr
+
+class FlaskrTest(unittest.TestCase):
+    def test_login_with_get_returns_400(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
