cbbe4276ca4912379b3ad1d9448a544e55b59783
Toby Wilkins <tobwilk@gmail.com>
2017-01-31 14:14:52 +0000

    added empty tests that will pass, fail and error

--- flaskr/tests.py
+++ flaskr/tests.py
@@ -3,9 +3,18 @@ import unittest
 import flaskr
 
 class FlaskrTest(unittest.TestCase):
-    def test_i_will_fail_for_no_reason(self):
-        # self.assertTrue(False)
+
+    def test_i_will_always_pass(self):
         self.assertTrue(True)
 
+    def test_i_will_always_fail(self):
+        self.assertTrue(False)
+
+    def test_i_will_always_error(self):
+        raise RuntimeError('Test error!')
+
+
+
 if __name__ == '__main__':
+    
     unittest.main()
