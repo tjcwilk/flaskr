90a4d1be075ff5c9da9e03fae61b922e836942f2
Toby Wilkins <tobwilk@gmail.com>
2017-01-31 14:28:50 +0000

    added a new testing group to demo fixtures

--- flaskr/tests.py
+++ flaskr/tests.py
@@ -47,6 +47,25 @@ class FlaskrTest(unittest.TestCase):
         #assertNotIsInstance(a, b, msg=None)
 
 
+
+
+class FixturesTest(unittest.TestCase):
+
+    def setUp(self):
+        print('In setUp()')
+        self.fixture = range(1, 10)
+
+    def tearDown(self):
+        print('In tearDown()')
+        del self.fixture
+
+    def test(self):
+        print('in test()')
+        self.assertEqual(self.fixture, range(1, 10))
+
+
+
+
 if __name__ == '__main__':
     
     unittest.main()
